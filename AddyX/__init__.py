9a466ae96f158ffa9da4c9ecca338289a6e23e34
AdarshuXD <108065687+AdarshuXD@users.noreply.github.com>
2023-04-13 09:38:38 +0530

    Add files via upload

--- AddyX/__init__.py
+++ AddyX/__init__.py
@@ -1,1 +1,37 @@
+from AddyX.core.bot import AddyXBot
+from AddyX.core.dir import dirr
+from AddyX.core.git import git
+from AddyX.core.userbot import Userbot
+from AddyX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
 
+from .logging import LOGGER
+
+
+dirr()
+
+git()
+
+dbb()
+
+heroku()
+
+sudo()
+
+# Clients
+app = AddyXBot()
+
+userbot = Userbot()
+
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
